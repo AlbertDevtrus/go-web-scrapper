76232d8bba5b1c9cbb2869716962b2faad5deb51
Luis Guzman <albertpatrsu@gmail.com>
2025-04-19 22:44:42 -0500

    Add function to get all the anchor links on a page

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AlbertDevtrus/go-web-scrapper
+
+go 1.24.2
+
+require golang.org/x/net v0.39.0
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+const base_url = "https://scrape-me.dreamsofcode.io"
+
+func main() {
+	response, err := Get(base_url)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	urlList := GetUrlList(response)
+
+	fmt.Print(urlList)
+
+}
+
+func Get(url string) (response *http.Response, err error) {
+	response, err = http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func GetUrlList(response *http.Response) (urlList []string) {
+
+	tokenizer := html.NewTokenizer(response.Body)
+
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			break
+		}
+
+		token := tokenizer.Token()
+
+		if tokenType == html.StartTagToken && token.Data == "a" {
+
+			for _, attr := range token.Attr {
+				if attr.Key == "href" {
+					url := attr.Val
+					if url[0] == '/' {
+						url = base_url + attr.Val
+					}
+					urlList = append(urlList, url)
+				}
+			}
+
+		}
+
+	}
+
+	return urlList
+}
